3af2abfdf66be86bd3751403349dc8d1e1346a0d
Abhilash <abhilash@heidelsoft-technologies.de>
2019-03-07 17:27:52 +0530

    Alternative method

--- src/app/call-information-form/call-information-form.component.ts
+++ src/app/call-information-form/call-information-form.component.ts
@@ -8,9 +8,11 @@ import { OperatorRatesService } from './../service/operator-rates.service';
 })
 export class CallInformationFormComponent implements OnInit {
   OperatorRates: Array<any>;
-  phoneNumber: string = '4673212345';
-  bestOperator: string;
-  bestMatch: string;
+  phoneNumber: string = '4611111111';
+  parsedRates: Array<any>;
+  dataProcessed: boolean = false;
+  cheepestOperator: string;
+  cheepestOperatorCost: number;
 
   constructor(private operatorRatesService: OperatorRatesService) {}
 
@@ -24,42 +26,61 @@ export class CallInformationFormComponent implements OnInit {
   }
 
   findBestOperator() {
-    if (this.phoneNumber) {
-// tslint:disable-next-line: forin
-      for (const operator in this.OperatorRates) {
-        // console.log(operator);
-        // console.log(this.OperatorRates[operator]);
-        // tslint:disable-next-line: forin
-        for (const rates in this.OperatorRates[operator]) {
-          // console.log(this.OperatorRates.operator[operator][rates]);
-          this.checkOperatorCost(operator, this.OperatorRates[operator][rates]);
-        }
-      }
-    } else {
-      console.log('Please enter a valid Telephone numbers');
+    if (!this.dataProcessed) {
+      this.prepareData();
+      this.sortRatesArray(this.parsedRates,'number_prefix');
+      this.dataProcessed = true;
     }
+    this.findCheepestOption();
   }
 
-  checkOperatorCost(operator, OperatorRates) {
-    // console.log(OperatorRates);
-    // console.log(OperatorRates['number_prefix'], '-' , OperatorRates['cost']);
-    // console.log(this.phoneNumber.substr(0, OperatorRates['number_prefix'].length));
-    const matchPrefix = this.phoneNumber.substr(0, OperatorRates['number_prefix'].length);
-    if (matchPrefix === OperatorRates['number_prefix']) {
-      //console.log(matchPrefix);
-      if (this.bestOperator) {
-        if (matchPrefix.length > this.bestMatch.length) {
-          this.bestOperator = operator;
-          this.bestMatch = matchPrefix;
-        } else if (matchPrefix.length === this.bestMatch.length) {
-          // con
-        }
-        console.log('bestOperator match: ', this.bestMatch);
-      } else {
-        // console.log('bestOperator: ', this.bestOperator);
-        this.bestOperator = operator;
-        this.bestMatch = matchPrefix;
+  prepareData() {
+    const clonedOperatorRate = { ...this.OperatorRates };
+    const tempArray = [];
+    // tslint:disable-next-line: forin
+    for (const operator in clonedOperatorRate) {
+      // tslint:disable-next-line: forin
+      for (const rates in clonedOperatorRate[operator]) {
+        clonedOperatorRate[operator][rates]['operator'] = operator;
+        tempArray.push(clonedOperatorRate[operator][rates]);
       }
+    }
+    this.parsedRates = tempArray;
+  }
+
+  sortRatesArray(sortArray, props) {
+    // this.parsedRates.sort((a, b) => {
+    //   return b.number_prefix - a.number_prefix;
+    // });
+    sortArray.sort((a, b) => {
+      if (props === 'cost') {
+        return b.cost - a.cost;
+      } else if (props === 'number_prefix') {
+        return b.number_prefix - a.number_prefix;
+      }
+    });
+  }
+
+  findCheepestOption() {
+    const bestOperatorMatch = [];
+    let foundRange = false;
+    for (let num = 0; num <= 9; num++) {
+      const partialPhoneNumber = this.phoneNumber.substr(0, this.phoneNumber.length - num);
+      // tslint:disable-next-line: forin
+      for (const rate in this.parsedRates) {
+        if (this.parsedRates[rate].number_prefix === partialPhoneNumber) {
+          bestOperatorMatch.push(this.parsedRates[rate]);
+          foundRange = true;
+        }
+      }
+      if(foundRange === true) {
+        break;
+      }
+    }
+    this.sortRatesArray(bestOperatorMatch, 'cost');
+    if (bestOperatorMatch.length > 0) {
+      this.cheepestOperator = bestOperatorMatch[0].operator;
+      this.cheepestOperatorCost = bestOperatorMatch[0].cost;
     }
   }
 }
